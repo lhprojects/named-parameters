412e7c74f1e746d2f3e77451705ea317a7c57474
Harry Liang <lihawl1009@gmail.com>
2021-09-21 05:06:55 +0800

    Update noncopyable.cpp

--- example/noncopyable.cpp
+++ example/noncopyable.cpp
@@ -8,7 +8,9 @@ struct NonCopyable {
 };
 
 namespace use_copyable_par {
-    static np::Parameter<1, NonCopyable> a1;
+    static np::Parameter<1, NonCopyable &> a1;
+    static np::Parameter<2, NonCopyable const &> a2;
+    static np::Parameter<3, NonCopyable &&> a3;
 };
 
 template<NP_ARGUMENT Arg>
@@ -21,13 +23,14 @@ void test_bar()
     using namespace use_copyable_par;
     NonCopyable bar_;
     use_copyable(a1 = bar_);
-    use_copyable(a1 = std::move(bar_));
+    use_copyable(a2 = bar_);
+    use_copyable(a3 = std::move(bar_));
     NonCopyable const const_bar;
-    use_copyable(a1 = const_bar);
-    use_copyable(a1 = static_cast<const NonCopyable&&>(const_bar));
+    use_copyable(a2 = const_bar);
 }
 
-int main() {
+int main()
+{
     test_bar();
     return 0;
 }
